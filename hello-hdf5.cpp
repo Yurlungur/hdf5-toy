142a2b74011b2feff3b6c2b2e413c44a1ce4cbe4
Jonah Miller <jonah.maxwell.miller@gmail.com>
2015-02-26 22:56:03 -0500

    First commit

---
+++ hello-hdf5.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <H5Cpp.h>
+#define DATATYPE H5::PredType::NATIVE_INT
+
+// these can ben changed later
+const std::string FILENAME("test.h5"); 
+const std::string FIELDNAME("scalarField");
+const std::string TIMENAME("time");
+const std::string DATANAME("/data");
+const std::string PROPERTIESNAME("/elementProperties");
+const int TNDIMS = 1;
+const int NDIMS = 3;
+const int DIMS[] = {3,3,3};
+int main() {
+  int data[DIMS[0]][DIMS[1]][DIMS[2]];
+  int times[DIMS[0]];
+  int count = 0;
+  for (int i=0; i < DIMS[0]; i++) {
+    times[i] = i;
+    for (int j = 0; j < DIMS[1]; j++) {
+      for (int k = 0; k < DIMS[2]; k++) {
+	count++;
+	data[i][j][k] = count;
+	}
+    }
+  }
+
+  hsize_t dimsf[NDIMS];
+  hsize_t dimst[TNDIMS];
+  for (int i = 0; i < NDIMS; i++) {
+    dimsf[i] = DIMS[i];
+  }
+  for (int i = 0; i < TNDIMS; i++) {
+    dimst[i] = dimsf[i];
+  }
+  // The file
+  H5::H5File file(FILENAME,H5F_ACC_TRUNC);
+  // The scalar field
+  H5::DataSpace fieldDataspace(NDIMS,dimsf);
+  H5::IntType datatype(DATATYPE);
+  H5::Group propertiesGroup(file.createGroup(PROPERTIESNAME));
+  H5::Group dataGroup(file.createGroup(DATANAME));
+  H5::DataSet fieldDataset = dataGroup.createDataSet(FIELDNAME,
+						     datatype,
+						     fieldDataspace);
+  fieldDataset.write(data,DATATYPE);
+  fieldDataset.close();
+  // The times
+  H5::DataSpace timeDataspace(TNDIMS,dimst);
+  H5::DataSet timeDataset = dataGroup.createDataSet(TIMENAME,
+						    datatype,
+						    timeDataspace);
+  timeDataset.write(times,DATATYPE);
+  timeDataset.close();
+  file.close();
+  return 0;
+}
